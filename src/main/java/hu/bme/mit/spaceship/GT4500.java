d0f688e1a1f3f5d953710531903a295ad1aa0567
Blextor <kokobobo2001@gmail.com>
2022-05-06 13:24:55 +0200

    | vagy || nem mindegy

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -88,11 +88,11 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         if (! primaryTorpedoStore.isEmpty()){
-          firingSuccess = firingSuccess || primaryTorpedoStore.fire(1);
+          firingSuccess = firingSuccess | primaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = true;
         }
         if (! secondaryTorpedoStore.isEmpty()){
-          firingSuccess = firingSuccess || secondaryTorpedoStore.fire(1);
+          firingSuccess = firingSuccess | secondaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = false;
         }
 
